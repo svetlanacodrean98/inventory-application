dc13bb93543b86631e560b57acd97deaf4017944
Svetlana Codrean <codrean.sveta@gmail.com>
2026-03-06 11:20:54 +0100

    fix styles.css path

--- controllers/drinkController.js
+++ controllers/drinkController.js
@@ -2,15 +2,14 @@ const db = require("../db");
 const CustomNotFoundError = require("../errors/CustomNotFoundError");
 
 async function getDrinkById(req, res) {
-    const { drinkId } = req.params;
 
+    const { drinkId } = req.params;
     const drink = await db.getDrinkById(Number(drinkId));
 
     if (!drink) {
         throw new CustomNotFoundError("Drink not found");
     }
 
-    // res.send(`Drink Name: ${drink.name}`);
     res.render("drink", {
         title: "Drink",
         links: db.getAllLinks(), 
